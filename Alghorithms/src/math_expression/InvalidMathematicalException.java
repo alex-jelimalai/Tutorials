af37cc82cfa88796de56cff1574143578c63cbfe
alex-jelimalai <alex.jeli@mail.ru>
2013-04-08 10:28:56 +0300

    Rename a file

---
+++ Alghorithms/src/math_expression/InvalidMathematicalException.java
@@ -0,0 +1,10 @@
+package math_expression;
+
+public class InvalidMathematicalException
+        extends RuntimeException {
+
+    public InvalidMathematicalException(String message) {
+        super(message);
+    }
+
+}
